c449f02a605b8283de2b967edffb393bce13e390
Pratama Bayu Widagdo <hi@pratamabayu.com>
2022-01-13 18:05:53 +0700

    Add dark mode

--- build.sh
+++ build.sh
@@ -3,5 +3,9 @@
 sass graff.scss graff.css
 sass graff.scss graff.min.css --style=compressed
 
+sass graff@dark.scss graff@dark.css
+sass graff@dark.scss graff@dark.min.css --style=compressed
+
 # copy to docs
 cp graff.min.css docs/themes/dooc/static/css
+cp graff@dark.min.css docs/themes/dooc/static/css
